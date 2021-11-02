4f1cc67b2f25fdc3d08f5c562820630a033ed686
Danilo da Silva Souza <danilosilva441@gmail.com>
2021-11-02 14:17:40 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/danilosilva441" target="_blank" rel="noreferrer">Danilo da Silva Souza</a></p>
+                <p className="text-light">Redes Sociais: <a href="https://www.linkedin.com/in/danilo-da-silva-souza-9b04a6140/" target="_blank" rel="noreferrer">Linkedin</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
